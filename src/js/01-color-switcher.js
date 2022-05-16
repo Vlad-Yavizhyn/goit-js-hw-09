6296ba197819acebd74d8d6c473c82607ad3ccbf
Vladlen <91382235+Vlad-Yavizhyn@users.noreply.github.com>
2022-05-16 18:32:02 +0300

    color done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
+
+let idInterval = null;
+
+const onChangeColor = () => {
+    onBtnDisabled()
+    idInterval = setInterval(() => {
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = `${color}`;
+    }, 1000)
+};
+
+const onStopChangeColor = () => {
+    clearInterval(idInterval);
+    onBtnDisabled()
+};
+
+function onBtnDisabled () {
+  if (!btnStartRef.disabled) {
+    btnStartRef.disabled = true;
+    btnStopRef.disabled = false
+  } else {
+    btnStartRef.disabled = false;
+    btnStopRef.disabled = true
+  }
+};
+
+btnStartRef.addEventListener("click", onChangeColor);
+btnStopRef.addEventListener("click", onStopChangeColor);
