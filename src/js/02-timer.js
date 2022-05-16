94eb82e92f4735ab026b532b5bdc8f6848d8b7c4
Vladlen <91382235+Vlad-Yavizhyn@users.noreply.github.com>
2022-05-16 19:14:21 +0300

    timer done

---
+++ src/js/02-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startButton: document.querySelector('button[data-start]'),
+  timerDays: document.querySelector('.value[data-days]'),
+  timerHours: document.querySelector('.value[data-hours]'),
+  timerMinutes: document.querySelector('.value[data-minutes]'),
+  timerSeconds: document.querySelector('.value[data-seconds]'),
+};
+
+const { input, startButton, timerDays, timerHours, timerMinutes, timerSeconds } = refs;
+
+startButton.setAttribute('disabled', 'disablet');
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+let intervalId = null;
+
+function onClose(selectedDates, dateStr, instance) {
+
+  const calculateTime = selectedDates[0] - instance.now;
+
+  if (calculateTime <= 0) {
+    Notify.failure('Please choose a date in the future');
+  }
+
+  if (calculateTime > 0) {
+    startButton.removeAttribute('disabled');
+    flatpickr(input, options);
+    const starTimer = () => {
+
+      intervalId = setInterval(() => {
+        const currentTime = Date.now();
+
+        const calcTimeUpatedInMs = selectedDates[0] - currentTime;
+
+        let calcTimeUpated = convertMs(calcTimeUpatedInMs);
+        const { days, hours, minutes, seconds } = calcTimeUpated
+
+        timerDays.textContent = days;
+        timerHours.textContent = hours;
+        timerMinutes.textContent = minutes;
+        timerSeconds.textContent = seconds;
+
+        if (calcTimeUpatedInMs < 999) {
+          clearInterval(intervalId);
+        }
+        console.log(calcTimeUpated);
+      }, 1000);
+    };
+
+    startButton.addEventListener('click', starTimer);
+  }
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose
+}
+
+flatpickr(input, options);
